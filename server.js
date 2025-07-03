94fa4394a3012a61df3f1fdec509eca7fe757d30
Chandrabali Biswas <chandrabalibiswas04@gmail.com>
2025-07-03 21:34:27 +0530

    Switch to Firebase key from env variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./firebaseKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
